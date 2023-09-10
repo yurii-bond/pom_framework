23597f6592e0f3a4b054403a61282f2ea215de52
Jack <selentsova12@gmail.com>
2023-09-10 14:18:57 +0200

    HW 21

--- tests/test_webdriver.py
+++ tests/test_webdriver.py
@@ -144,4 +144,5 @@ def test_status_codes():
     link.click()
     sleep(1)
     assert driver.current_url == "https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml"
+
     driver.quit()
